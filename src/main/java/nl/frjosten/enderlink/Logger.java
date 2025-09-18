383b0a54fa44ddba94fec888d55ddb75d07bf94e
Finn Josten <finn.r.josten@proton.me>
2025-09-18 10:00:50 +0200

    v1.1.1 small fix to add [EnderLink] to the loggers so its easier from what it comes

--- src/main/java/nl/frjosten/enderlink/Logger.java
+++ src/main/java/nl/frjosten/enderlink/Logger.java
@@ -3,18 +3,21 @@ package nl.frjosten.enderlink;
 import org.bukkit.Bukkit;
 
 public class Logger {
+    private String prefix = "[EnderLink] ";
+
     public void info(String msg) {
-        Bukkit.getLogger().info(msg);
+        Bukkit.getLogger().info(prefix + msg);
     }
 
     public void warning(String msg) {
-        Bukkit.getLogger().warning(msg);
+        Bukkit.getLogger().warning(prefix + msg);
     }
 
     public void error(String msg) {
-        Bukkit.getLogger().severe(msg);
+        Bukkit.getLogger().severe(prefix + msg);
     }
 
+    // Alias for error
     public void severe(String msg) {
         error(msg);
     }
